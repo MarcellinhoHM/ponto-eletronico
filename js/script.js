b21e8ea220ef74f487c8583148d20a63efb94622
MarcellinhoHM <marcellomontella@yahoo.com.br>
2024-09-02 10:44:03 -0300

    luiz eduardo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+});
+
 const date = new Date();
 const diaSemana = document.getElementById("dia-semana");
 const dataAtual = document.getElementById("data-atual");
 const horaAtual = document.getElementById("hora-atual");
+const btnRegister = document.getElementById("btn-register");
 
+btnRegister.addEventListener("click", register);
 
 function updateContentHour(){
     dataAtual.textContent = getCurrentDate();
     horaAtual.textContent = getCurrentTime();
     diaSemana.textContent = getWeekDay();
+}
+
+const dialogPonto = document.getElementById("dialog-ponto");
+
+const dialogData = document.getElementById("dialog-data");
+dialogData.textContent = getCurrentDate(); 
+
+const dialogHora = document.getElementById("dialog-hora");
+dialogHora.textContent = getCurrentTime(); 
+
+
+const btnDialogFechar = document.getElementById("dialog-fechar");
+btnDialogFechar.addEventListener("click", ()=> {
+    dialogPonto.close();
+})
+
+function register(){
+    alert("im doin' your mom");
+    dialogPonto.showModal();
 }
 
 function getCurrentTime() {
